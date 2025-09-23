d5bb8c80a1793171dec33a66df1177ed60991cea
Emilly Lemke <emilly.lais.lemke@gmail.com>
2025-09-22 23:52:07 -0300

    Delete src directory

--- src/main/java/com/emillyl/jpa/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.emillyl.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
-}
--- src/main/java/com/emillyl/jpa/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.emillyl.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-}
